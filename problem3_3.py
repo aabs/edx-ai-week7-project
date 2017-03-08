2916a509a46b8d37f276f6a61b899ff8e269b9e6
Andrew Matthews <matthews.andrew@gmail.com>
2017-03-09 10:53:50 +1100

    using grid search cross validation (2)

--- problem3_3.py
+++ problem3_3.py
@@ -17,7 +17,8 @@ from io_handling import open_output
 
 def import_and_scale_training_data(input_file_path, with_bias_column=False):
     raw_data = np.loadtxt(input_file_path, delimiter=',', skiprows=1)
-    x_train, x_test, y_train, y_test = train_test_split(raw_data[:, [0,1]], raw_data[:, [2]].flatten(), test_size=0.4, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(raw_data[:, [0, 1]], raw_data[:, [2]].flatten(), test_size=0.4,
+                                                        random_state=42)
     return x_train, x_test, y_train, y_test
 
 
@@ -29,126 +30,98 @@ def showGraph(data, l):
     plt.scatter(data[:, [1]], data[:, [2]], c=colormap[labs], s=20)
     plt.show()
 
-def measure_fitness(classifier, test_data, expectations):
-    classifier.decision_function(test_data)
-    predicted = classifier.predict(test_data)
-    score = 0
-    for x, a in zip(expectations, predicted):
-        if x == a:
-            score += 1
-    return score
+
+def report(output_stream, classifier_name, best_score, test_score):
+    print("%s, %0.2f, %0.2f" % (classifier_name, best_score, test_score))
+    # output_stream.write("%s, %0.2f, %0.2f" % (classifier_name, best_score, test_score))
 
 
-def report(of, alg, best_score, test_score):
-    print("%s, %0.2f, %0.2f" % (alg, best_score, test_score))
-    # of.write("%s, %0.2f, %0.2f" % (alg, best_score, test_score))
-
-
-def run_model(estimator, params, training_data, test_data, training_labels, test_labels, name, of):
+def run_model(estimator, params, training_data, test_data, training_labels, test_labels, classifier_name,
+              output_stream):
     clf = GridSearchCV(estimator, params, n_jobs=-1)
     clf.fit(training_data, training_labels)
-    bs = clf.best_score_
+    best_score = clf.best_score_
     test_score = clf.score(test_data, test_labels)
-    report(of, name, bs, test_score)
-
-
-def run_svm(kernel, C, degree, gamma, training_data, training_labels, test_data, test_labels):
-    param_grid = [
-        {'C': C, 'gamma': gamma, 'kernel': [kernel]},
-    ]
-    best_score = 0
-    for c in C:
-        for d in degree:
-            for g in gamma:
-                clf = svm.SVC(C=c, degree=d, gamma=g, kernel=kernel)
-                clf.fit(training_data, training_labels)
-                score = clf.score(test_data, test_labels)
-                best_score = max(best_score, score)
-    return best_score
+    report(output_stream, classifier_name, best_score, test_score)
 
 
 def svm_with_linear_kernel(training_data, training_labels, test_data, test_labels, of):
     params = {
-                'C': [0.1, 0.5, 1, 5, 10, 50, 100],
-                'degree': [4, 5, 6],
-                'gamma': [0.1, 1],
-                'kernel': ['linear']
+        'C': [0.1, 0.5, 1, 5, 10, 50, 100],
+        'degree': [4, 5, 6],
+        'gamma': [0.1, 1],
+        'kernel': ['linear']
     }
     run_model(SVC(), params, training_data, test_data, training_labels, test_labels, 'svm_linear', of)
 
 
 def svm_with_polynomial_kernel(training_data, training_labels, test_data, test_labels, of):
     params = {
-                'C': [0.1, 1, 3],
-                'degree': [4, 5, 6],
-                'gamma': [0.1, 1],
-                'kernel': ['poly']
+        'C': [0.1, 1, 3],
+        'degree': [4, 5, 6],
+        'gamma': [0.1, 1],
+        'kernel': ['poly']
     }
     run_model(SVC(), params, training_data, test_data, training_labels, test_labels, 'svm_polynomial', of)
 
 
 def svm_with_rbf_kernel(training_data, training_labels, test_data, test_labels, of):
     params = {
-                'C': [0.1, 0.5, 1, 5, 10, 50, 100],
-                'gamma': [0.1, 0.5, 1, 3, 6, 10],
-                'kernel': ['rbf']
+        'C': [0.1, 0.5, 1, 5, 10, 50, 100],
+        'gamma': [0.1, 0.5, 1, 3, 6, 10],
+        'kernel': ['rbf']
     }
     run_model(SVC(), params, training_data, test_data, training_labels, test_labels, 'svm_rbf', of)
 
 
 def logistic_regression(training_data, training_labels, test_data, test_labels, of):
     params = {
-                'C': [0.1, 0.5, 1, 5, 10, 50, 100],
-                'gamma': [0.1, 0.5, 1, 3, 6, 10],
-                'solver': 'liblinear'
+        'C': [0.1, 0.5, 1, 5, 10, 50, 100],
+        'solver': ['liblinear']
     }
     run_model(LogisticRegression(), params, training_data, test_data, training_labels, test_labels, 'logistic', of)
 
 
-
 def k_nearest_neighbors(training_data, training_labels, test_data, test_labels, of):
-    best_score = 0
-    for n_neighbours in range(1, 51):
-        for leaf_size in range(5, 65, 5):
-            clf = KNeighborsClassifier(algorithm = 'auto', n_neighbors=n_neighbours, leaf_size=leaf_size)
-            clf.fit(training_data, training_labels)
-            score = clf.score(test_data, test_labels)
-            best_score = max(best_score, score)
-    report(of, 'knn', best_score, 0)
-
+    params = {
+        'n_neighbors': range(1, 51),
+        'leaf_size': range(5, 65, 5),
+        'algorithm': ['auto']
+    }
+    run_model(KNeighborsClassifier(), params, training_data, test_data, training_labels, test_labels, 'knn', of)
 
 def decision_trees(training_data, training_labels, test_data, test_labels, of):
-    best_score = 0
-    for max_depth in range(1, 51):
-        for min_samples_split in range(2, 11):
-            clf = DecisionTreeClassifier(max_depth=max_depth, min_samples_split=min_samples_split)
-            clf.fit(training_data, training_labels)
-            score = clf.score(test_data, test_labels)
-            best_score = max(best_score, score)
-    report(of, 'decision_tree', best_score, 0)
-
+    params = {
+        'max_depth': range(1, 51),
+        'min_samples_split': range(2, 11)
+    }
+    run_model(DecisionTreeClassifier(), params, training_data, test_data, training_labels, test_labels, 'decision_tree', of)
 
 def random_forest(training_data, training_labels, test_data, test_labels, of):
-    best_score = 0
-    for max_depth in range(1, 51):
-        for min_samples_split in range(2, 11):
-            clf = RandomForestClassifier(max_depth=max_depth, min_samples_split=min_samples_split)
-            clf.fit(training_data, training_labels)
-            score = clf.score(test_data, test_labels)
-            best_score = max(best_score, score)
-    report(of, 'random_forest', best_score, 0)
+    params = {
+        'max_depth': range(1, 51),
+        'min_samples_split': range(2, 11)
+    }
+    run_model(RandomForestClassifier(), params, training_data, test_data, training_labels, test_labels, 'random_forest', of)
 
 
 def main():
     training_data, test_data, training_labels, test_labels = import_and_scale_training_data(sys.argv[1])
     # showGraph(data, labels)
     of = open_output(sys.argv[2])
-    funcs = [svm_with_linear_kernel, svm_with_polynomial_kernel, svm_with_rbf_kernel, logistic_regression,
-               k_nearest_neighbors, decision_trees, random_forest]
-    wip = [logistic_regression]
-    for fn in wip:
+    funcs = [
+          svm_with_linear_kernel
+        # , svm_with_polynomial_kernel
+        , svm_with_rbf_kernel
+        , logistic_regression
+        , k_nearest_neighbors
+        , decision_trees
+        , random_forest
+    ]
+    for fn in funcs:
         fn(training_data, training_labels, test_data, test_labels, of)
     of.close()
+
 
 if __name__ == "__main__":
     main()
